018e08bb68c33edce129b66b47c9c22d151072f0
adekniyi <adekniyi@gmail.com>
2020-03-24 10:03:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Adekunle Adeniyi";
+var courses = ["Html", "Css", "JavaScript", "C#", "Nodejs"];
+console.log(name);
+console.log(courses);
+var num = 200;
+
+for(i=0; i<= 200; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
